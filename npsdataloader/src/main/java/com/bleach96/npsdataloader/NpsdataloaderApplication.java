96149977ee04dee8cf5ef2a523509719d388a5e5
bleach96 <ibzy71@gmail.com>
2023-06-17 01:40:41 -0500

    Used Spring Boot Initializer to create general project

---
+++ npsdataloader/src/main/java/com/bleach96/npsdataloader/NpsdataloaderApplication.java
@@ -0,0 +1,13 @@
+package com.bleach96.npsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpsdataloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpsdataloaderApplication.class, args);
+	}
+
+}
---
+++ npsdataloader/src/test/java/com/bleach96/npsdataloader/NpsdataloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bleach96.npsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpsdataloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
